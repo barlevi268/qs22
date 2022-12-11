ff4a4ca82548a7d07d94ee9ded0e8ace03f6b94e
Bar Levi <bar@oltre.io>
2022-12-11 13:25:16 +0200

    don't specify env location

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,5 @@
 import { config } from 'dotenv';
-config({ path: '.env' });
+config();
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const {
-  NODE_ENV,
-  PORT,
-  SECRET_KEY,
-  LOG_FORMAT,
-  LOG_DIR,
-  ORIGIN,
-  PG_CONNECTION_STRING,
-  APP_URL,
-} = process.env;
+export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, PG_CONNECTION_STRING, APP_URL } = process.env;
